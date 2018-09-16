5afe38c2966351dc94e9fc62265ab27a847827af
Shivam Sinha <sinhashivam04@gmail.com>
2018-09-16 07:50:44 +0530

    mini changes

--- app/routes/health-records.js
+++ app/routes/health-records.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	beforeModel(/* transition */) {
-    	this.transitionTo('/health_records/disease_count'); // Implicitly aborts the on-going transition.
+    	//this.transitionTo('/health_records/disease_count'); // Implicitly aborts the on-going transition.
   	}
 });
